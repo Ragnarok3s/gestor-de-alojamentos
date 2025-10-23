f3a9c11ff16c1e3504030b8e570cc2a4b51376ba
João Gomes <keepcalm.ds@gmail.com>
2025-10-23 12:34:35 +0100

    Use upstream dayjs dependency

--- vendor/dayjs/plugin/customParseFormat.js
+++
@@ -1,6 +0,0 @@
-module.exports = function customParseFormatPlugin(options, _Dayjs, dayjs) {
-  dayjs.p.customParseFormat = true;
-  if (typeof dayjs.__setParseTwoDigitYear === 'function' && options && Object.prototype.hasOwnProperty.call(options, 'parseTwoDigitYear')) {
-    dayjs.__setParseTwoDigitYear(options.parseTwoDigitYear);
-  }
-};
